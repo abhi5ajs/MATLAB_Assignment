6183979287e5489cf9b645d09362458fbe921828
abhi5ajs <87848080+abhi5ajs@users.noreply.github.com>
2021-08-16 11:05:17 +0530

    Add files via upload

--- GenerateImages_v8.m
+++ GenerateImages_v8.m
@@ -6,10 +6,10 @@ beta = 8/3;
 rho = 50;
 ObjectSize = 10;
 %% User define the parameter for generating images with noise
-Noise     = input('Please enter whether Stake of images is required with Noise: '); %Without noise = 0, With Noise =1;
-d         = input('Please enter the value of d: '); %Default = 0.05
-m         = input('Please enter the mean number: '); %Default = 0
-var_gauss = input('Please enter the guassian variance number: '); %Default = 0.01
+noise         = input('Please enter whether Stake of images is required with noise: '); %Without noise = 0, With Noise =1;
+noise_density = input('Please enter the value of noise_density: '); %Default = 0.05
+noise_mean    = input('Please enter the noise mean number: '); %Default = 0
+var_gauss     = input('Please enter the guassian variance number: '); %Default = 0.01
 %% Dynamics of Lorentz attractor
 s = 50; % Level of random excitation -- for. e.g. due to turbulence.
 f = @(t,a)[-sigma*a(1) + sigma*a(2) + s*randn;
@@ -42,8 +42,6 @@ if ~isfolder(myFolder)
     end
 end
 
-
-
 %% Generate Stakes of Images and Save with Noises
 for i = 1:length(t)
     figure(2); clf;
@@ -60,85 +58,6 @@ for i = 1:length(t)
     set(gcf,'InvertHardCopy','Off', 'Color', [0.5 0.5 0.5])
     fname_1 = sprintf('images1/frame%d.png',i);
     saveas(gcf,'fname_1.png');
-    IMAGE = imread('fname_1.png');
-    imshow(IMAGE);
-    
-    [salt, gau, mynoise] = Trial_7;
-    
-%     if Noise == 0
-%         original_pic_2 = IMAGE;
-%         imshow(original_pic_2)
-%         title('Image Without Noise');
-%         fname_2 = sprintf('images2/frame%d.png',i);
-%         saveas(gcf,'fname_2.png');
-%         IMAGE_1 = imread('fname_2.png');
-%         figure(2);
-%         Output = IMAGE_1;
-%         drawnow; % Force display to update immediately.
-%         imwrite(Output ,sprintf('IMAGE_Without_0%d.png',i)); % will create mask1, mask2,...
-%     elseif Noise == 1 % If we add different valur of "d"
-%         original_pic_3 = IMAGE;
-%         salt = imnoise(original_pic_3, 'salt & pepper', d);% adds salt and pepper noise, where d is the noise density. This affects approximately d*numel(Image_1) pixels.
-%         imshow(salt);
-%         title('Salt & Pepper Image');
-%         fname_3 = sprintf('images3/frame%d.png',i);
-%         saveas(gcf,'fname_3.png');
-%         figure(3);
-%         IMAGE_2 = imread('fname_3.png');
-%         Output = IMAGE_2;
-%         drawnow; % Force display to update immediately.
-%         imwrite(Output ,sprintf('IMAGE_With_Salt&Pepper_Noise_0%d.png',i)); % will create mask1, mask2,...
-%     elseif Noise == 2
-%         original_pic_4 = IMAGE;
-%         gau = imnoise(original_pic_4, 'gaussian');%adds zero-mean, Gaussian white noise with variance of 0.01 to grayscale image_1.
-%         imshow(gau);
-%         title('Gaussian Image');
-%         fname_4 = sprintf('images4/frame%d.png',i);
-%         saveas(gcf,'fname_4.png');
-%         figure(3);
-%         IMAGE_4 = imread('fname_4.png');
-%         Output = IMAGE_4;
-%         drawnow; % Force display to update immediately.
-%         imwrite(Output ,sprintf('IMAGE_With_Gaussian_Noise_0%d.png',i)); % will create mask1, mask2,...
-%     elseif Noise == 3
-%         original_pic_5 = IMAGE;
-%         mynoise = m + sqrt(var_gauss)*randn(size(original_pic_5)); %adds Gaussian white noise with mean m and variance var_gauss.
-%         imshow(mynoise,[]);
-%         title('Generated Gaussian Noise');
-%         fname_5 = sprintf('images5/frame%d.png',i);
-%         saveas(gcf,'fname_5.png');
-%         figure(4);
-%         IMAGE_5 = imread('fname_5.png');
-%         Output = IMAGE_5;
-%         drawnow; % Force display to update immediately.
-%         imwrite(Output ,sprintf('IMAGE_With_Generated_Gaussian_Noise_0%d.png',i)); % will create mask1, mask2,...
-%     elseif Noise == 4
-%         original_pic_6 = IMAGE;
-%         salt = imnoise(original_pic_6, 'salt & pepper', d);
-%         gau = imnoise(original_pic_6, 'gaussian');
-%         SP_GN = (salt + gau)/2;
-%         imshow(SP_GN);
-%         title('Salt & Pepper + Gaussian Noise');
-%         fname_5 = sprintf('images6/frame%d.png',i);
-%         saveas(gcf,'fname_6.png');
-%         figure(4);
-%         IMAGE_6 = imread('fname_6.png');
-%         Output = IMAGE_6;
-%         drawnow; % Force display to update immediately.
-%         imwrite(Output ,sprintf('IMAGE_With_Salt&Pepper_and_Gaussian_Noise_0%d.png',i)); % will create mask1, mask2,...
-%     else Noise == 5;
-%         original_pic_7 = IMAGE;
-%         salt = imnoise(original_pic_7, 'salt & pepper', d);
-%         mynoise = imnoise(original_pic_7, 'gaussian', m, var_gauss);
-%         SP_GGN = (salt + mynoise)/2;
-%         imshow(SP_GGN);
-%         title('Salt & Pepper + Generated Gaussian Noise');
-%         fname_5 = sprintf('images7/frame%d.png',i);
-%         saveas(gcf,'fname_7.png');
-%         figure(4);
-%         IMAGE_7 = imread('fname_7.png');
-%         Output = IMAGE_7;
-%         drawnow; % Force display to update immediately.
-%         imwrite(Output ,sprintf('IMAGE_With_Salt&Pepper_and_Generated_Gaussian_Noise_0%d.png',i)); % will create mask1, mask2,...
-%     end
+
+    [salt, gau, mynoise] = Trial_7(noise, noise_mean, noise_density, var_gauss);
 end
--- Trial_7.m
+++ Trial_7.m
@@ -1,81 +1,88 @@
-function [salt, gau, mynoise] = Trial_7(Noise, m, d, var_gauss)
+function [salt, gau, mynoise] = Trial_7(noise, noise_mean, noise_density, var_gauss)
 % Features -> Using imread to read the jpeg
 %%
 % User define the parameter for generating images with noise
-    if Noise == 0
-        original_pic_2 = IMAGE;
-        imshow(original_pic_2)
-        title('Image Without Noise');
-        fname_2 = sprintf('images2/frame%d.png',i);
-        saveas(gcf,'fname_2.png');
-        IMAGE_1 = imread('fname_2.png');
-        figure(2);
-        Output = IMAGE_1;
-        drawnow; % Force display to update immediately.
-        imwrite(Output ,sprintf('IMAGE_Without_0%d.png',i)); % will create mask1, mask2,...
-    elseif Noise == 1 % If we add different valur of "d"
-        original_pic_3 = IMAGE;
-        salt = imnoise(original_pic_3, 'salt & pepper', d);% adds salt and pepper noise, where d is the noise density. This affects approximately d*numel(Image_1) pixels.
-        imshow(salt);
-        title('Salt & Pepper Image');
-        fname_3 = sprintf('images3/frame%d.png',i);
-        saveas(gcf,'fname_3.png');
-        figure(3);
-        IMAGE_2 = imread('fname_3.png');
-        Output = IMAGE_2;
-        drawnow; % Force display to update immediately.
-        imwrite(Output ,sprintf('IMAGE_With_Salt&Pepper_Noise_0%d.png',i)); % will create mask1, mask2,...
-    elseif Noise == 2
-        original_pic_4 = IMAGE;
-        gau = imnoise(original_pic_4, 'gaussian');%adds zero-mean, Gaussian white noise with variance of 0.01 to grayscale image_1.
-        imshow(gau);
-        title('Gaussian Image');
-        fname_4 = sprintf('images4/frame%d.png',i);
-        saveas(gcf,'fname_4.png');
-        figure(3);
-        IMAGE_4 = imread('fname_4.png');
-        Output = IMAGE_4;
-        drawnow; % Force display to update immediately.
-        imwrite(Output ,sprintf('IMAGE_With_Gaussian_Noise_0%d.png',i)); % will create mask1, mask2,...
-    elseif Noise == 3
-        original_pic_5 = IMAGE;
-        mynoise = m + sqrt(var_gauss)*randn(size(original_pic_5)); %adds Gaussian white noise with mean m and variance var_gauss.
-        imshow(mynoise,[]);
-        title('Generated Gaussian Noise');
-        fname_5 = sprintf('images5/frame%d.png',i);
-        saveas(gcf,'fname_5.png');
-        figure(4);
-        IMAGE_5 = imread('fname_5.png');
-        Output = IMAGE_5;
-        drawnow; % Force display to update immediately.
-        imwrite(Output ,sprintf('IMAGE_With_Generated_Gaussian_Noise_0%d.png',i)); % will create mask1, mask2,...
-    elseif Noise == 4
-        original_pic_6 = IMAGE;
-        salt = imnoise(original_pic_6, 'salt & pepper', d);
-        gau = imnoise(original_pic_6, 'gaussian');
-        SP_GN = (salt + gau)/2;
-        imshow(SP_GN);
-        title('Salt & Pepper + Gaussian Noise');
-        fname_5 = sprintf('images6/frame%d.png',i);
-        saveas(gcf,'fname_6.png');
-        figure(4);
-        IMAGE_6 = imread('fname_6.png');
-        Output = IMAGE_6;
-        drawnow; % Force display to update immediately.
-        imwrite(Output ,sprintf('IMAGE_With_Salt&Pepper_and_Gaussian_Noise_0%d.png',i)); % will create mask1, mask2,...
-    else Noise == 5;
-        original_pic_7 = IMAGE;
-        salt = imnoise(original_pic_7, 'salt & pepper', d);
-        mynoise = imnoise(original_pic_7, 'gaussian', m, var_gauss);
-        SP_GGN = (salt + mynoise)/2;
-        imshow(SP_GGN);
-        title('Salt & Pepper + Generated Gaussian Noise');
-        fname_5 = sprintf('images7/frame%d.png',i);
-        saveas(gcf,'fname_7.png');
-        figure(4);
-        IMAGE_7 = imread('fname_7.png');
-        Output = IMAGE_7;
-        drawnow; % Force display to update immediately.
-        imwrite(Output ,sprintf('IMAGE_With_Salt&Pepper_and_Generated_Gaussian_Noise_0%d.png',i)); % will create mask1, mask2,...    
-    end
+image = imread('fname_1.png');
+imshow(image);
+
+if noise == 0
+    original_pic_2 = image;
+    imshow(original_pic_2);
+    title('image Without noise');
+    fname_2 = sprintf('images2/frame%d.png',i);
+    saveas(gcf,'fname_2.png');
+    image_1 = imread('fname_2.png');;
+    figure(2);
+    Output = image_1;
+    drawnow; % Force display to update immediately.
+    imwrite(Output ,sprintf('image_Without_0%d.png',i)); % will create mask1, mask2,...
+
+elseif noise == 1 % If we add different valur of "d"
+    original_pic_3 = image;
+    salt = imnoise(original_pic_3, 'salt & pepper', noise_density);% adds salt and pepper noise, where d is the noise density. This affects approximately d*numel(image_1) pixels.
+    imshow(salt);
+    title('Salt & Pepper image');
+    fname_3 = sprintf('images3/frame%d.png',i);
+    saveas(gcf,'fname_3.png');
+    figure(3);
+    image_2 = imread('fname_3.png');
+    Output = image_2;
+    drawnow; % Force display to update immediately.
+    imwrite(Output ,sprintf('image_With_Salt&Pepper_noise_0%d.png',i)); % will create mask1, mask2,...
+
+elseif noise == 2
+    original_pic_4 = image;
+    gau = imnoise(original_pic_4, 'gaussian');%adds zero-mean, Gaussian white noise with variance of 0.01 to grayscale image_1.
+    imshow(gau);
+    title('Gaussian image');
+    fname_4 = sprintf('images4/frame%d.png',i);
+    saveas(gcf,'fname_4.png');
+    figure(3);
+    image_4 = imread('fname_4.png');
+    Output = image_4;
+    drawnow; % Force display to update immediately.
+    imwrite(Output ,sprintf('image_With_Gaussian_noise_0%d.png',i)); % will create mask1, mask2,...
+
+elseif noise == 3
+    original_pic_5 = image;
+    mynoise = noise_mean + sqrt(var_gauss)*randn(size(original_pic_5)); %adds Gaussian white noise with mean m and variance var_gauss.
+    imshow(mynoise,[]);
+    title('Generated Gaussian noise');
+    fname_5 = sprintf('images5/frame%d.png',i);
+    saveas(gcf,'fname_5.png');
+    figure(4);
+    image_5 = imread('fname_5.png');
+    Output = image_5;
+    drawnow; % Force display to update immediately.
+    imwrite(Output ,sprintf('image_With_Generated_Gaussian_noise_0%d.png',i)); % will create mask1, mask2,...
+elseif noise == 4
+    original_pic_6 = image;
+    salt = imnoise(original_pic_6, 'salt & pepper', noise_density);
+    gau = imnoise(original_pic_6, 'gaussian');
+    SP_GN = (salt + gau)/2;
+    imshow(SP_GN);
+    title('Salt & Pepper + Gaussian noise');
+    fname_5 = sprintf('images6/frame%d.png',i);
+    saveas(gcf,'fname_6.png');
+    figure(4);
+    image_6 = imread('fname_6.png');
+    Output = image_6;
+    drawnow; % Force display to update immediately.
+    imwrite(Output ,sprintf('image_With_Salt&Pepper_and_Gaussian_noise_0%d.png',i)); % will create mask1, mask2,...
+
+else noise == 5;
+    original_pic_7 = image;
+    salt = imnoise(original_pic_7, 'salt & pepper', noise_density);
+    mynoise = imnoise(original_pic_7, 'gaussian', noise_mean, var_gauss);
+    SP_GGN = (salt + mynoise)/2;
+    imshow(SP_GGN);
+    title('Salt & Pepper + Generated Gaussian noise');
+    fname_5 = sprintf('images7/frame%d.png',i);
+    saveas(gcf,'fname_7.png');
+    figure(4);
+    image_7 = imread('fname_7.png');
+    Output = image_7;
+    drawnow; % Force display to update immediately.
+    imwrite(Output ,sprintf('image_With_Salt&Pepper_and_Generated_Gaussian_noise_0%d.png',i)); % will create mask1, mask2,...
+end
 end
